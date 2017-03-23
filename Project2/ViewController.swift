1b7121f0fce6467952f703ee369086e5e6a89c7c
eldooon <eldonchan91@gmail.com>
2017-03-22 22:34:58 -0400

    Added images, added addtopping subview

--- Project2/ViewController.swift
+++ Project2/ViewController.swift
@@ -7,8 +7,15 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
+    
+    let pizzaImageView = UIImageView()
+    let addToppingButton = UIButton()
+    let pepperoniButton = UIButton()
+    let cheeseButton = UIButton()
+    let mushroomButton = UIButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,11 +30,37 @@ class ViewController: UIViewController {
 
     func createLayout() {
         
-        //Navigationbar properties
+        //Navigation bar properties
         self.navigationController?.navigationBar.barStyle = UIBarStyle.default
         self.navigationItem.leftBarButtonItem = UIBarButtonItem(image: AgentSuiteStyle.imageOfMoreButton, style: .done, target: self, action: nil)
         self.navigationController?.navigationBar.tintColor = UIColor.black
         self.title = "Ms.Pizza"
+        
+        //Pizza image view properties
+        view.addSubview(pizzaImageView)
+        pizzaImageView.image = UIImage(named: "pizza")
+        pizzaImageView.snp.makeConstraints { (make) in
+            make.top.equalTo(view.snp.top).offset(100)
+            make.centerX.equalTo(view.snp.centerX)
+            make.width.equalTo(view.snp.width).dividedBy(2)
+            make.height.equalTo(view.snp.width).dividedBy(2)
+        }
+        
+        //addTopping image view properties
+        view.addSubview(addToppingButton)
+        addToppingButton.setImage(UIImage(named: "expand"), for: .normal)
+        addToppingButton.snp.makeConstraints { (make) in
+            make.top.equalTo(pizzaImageView.snp.bottom).offset(200)
+            make.centerX.equalTo(view.snp.centerX)
+            make.width.equalTo(view.snp.width).dividedBy(10)
+            make.height.equalTo(view.snp.width).dividedBy(10)
+        }
+        
+        
+    }
+    
+    func addToppingButtonTapped() {
+        
     }
 
 }
